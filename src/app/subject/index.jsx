03bb50a2632b2c97fa711d68fbc344d429367ed5
Saraph1nes <xiayx.leo@qq.com>
2024-03-20 17:21:40 +0800

    feat: 专题页增加设计模式

--- src/app/subject/index.jsx
+++ src/app/subject/index.jsx
@@ -1,5 +1,5 @@
-import {Paper, Stack} from "@mui/material";
-import {useContext} from "react";
+import {Paper} from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
 import {useNavigate} from "react-router-dom";
 
 import './index.scss'
@@ -17,41 +17,54 @@ const Subject = () => {
     if (type === 'algorithm') {
       navigate('/book/14')
     }
+    if (type === 'designPatterns') {
+      navigate('/book/17')
+    }
   }
 
   return <div className='page-subject'>
     <section className='page-subject-section'>
       <h2>技术专题</h2>
-      <Stack
-        className='special-subject-list'
-        direction={'row'}
-        spacing={3}
-      >
-        <Paper
-          square={false}
-          className="special-subject-list-item frontend"
-          onClick={() => onPaperClick('frontend')}
-        >
-          <h3 className='special-subject-list-item-title'>前端</h3>
-          <img
-            className='special-subject-list-item-img'
-            src="https://assest.sablogs.cn/imgs/blog/React.png"
-            alt=""
-          />
-        </Paper>
-        <Paper square={false} className="special-subject-list-item backend"
-               onClick={() => onPaperClick('backend')}>
-          <h3 className='special-subject-list-item-title'>后端</h3>
-          <img className='special-subject-list-item-img' src="https://assest.sablogs.cn/imgs/blog/nodejs.png"
-               alt=""/>
-        </Paper>
-        <Paper square={false} className="special-subject-list-item algorithm"
-               onClick={() => onPaperClick('algorithm')}>
-          <h3 className='special-subject-list-item-title'>算法</h3>
-          <img className='special-subject-list-item-img' src="https://assest.sablogs.cn/imgs/blog/suanfa.png"
-               alt=""/>
-        </Paper>
-      </Stack>
+      <Grid container spacing={2} className='special-subject-list'>
+        <Grid xs={4}>
+          <Paper
+            square={false}
+            className="special-subject-list-item frontend"
+            onClick={() => onPaperClick('frontend')}
+          >
+            <h3 className='special-subject-list-item-title'>前端专题</h3>
+            {/*<img*/}
+            {/*  className='special-subject-list-item-img'*/}
+            {/*  src="https://assest.sablogs.cn/imgs/blog/React.png"*/}
+            {/*  alt=""*/}
+            {/*/>*/}
+          </Paper>
+        </Grid>
+        <Grid xs={4}>
+          <Paper square={false} className="special-subject-list-item backend"
+                 onClick={() => onPaperClick('backend')}>
+            <h3 className='special-subject-list-item-title'>后端专题</h3>
+            {/*<img className='special-subject-list-item-img' src="https://assest.sablogs.cn/imgs/blog/nodejs.png"*/}
+            {/*     alt=""/>*/}
+          </Paper>
+        </Grid>
+        <Grid xs={4}>
+          <Paper square={false} className="special-subject-list-item algorithm"
+                 onClick={() => onPaperClick('algorithm')}>
+            <h3 className='special-subject-list-item-title'>算法专题</h3>
+            {/*<img className='special-subject-list-item-img' src="https://assest.sablogs.cn/imgs/blog/suanfa.png"*/}
+            {/*     alt=""/>*/}
+          </Paper>
+        </Grid>
+        <Grid xs={4}>
+          <Paper square={false} className="special-subject-list-item designPatterns"
+                 onClick={() => onPaperClick('designPatterns')}>
+            <h3 className='special-subject-list-item-title'>设计模式专题</h3>
+            {/*<img className='special-subject-list-item-img' src="https://assest.sablogs.cn/imgs/blog/suanfa.png"*/}
+            {/*     alt=""/>*/}
+          </Paper>
+        </Grid>
+      </Grid>
     </section>
   </div>
 }
